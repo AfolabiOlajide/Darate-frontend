7582ffeaee98c9a5d166bfd225b305a8b7f89c51
Afolabi Olajide Samuel <samolajide20@gmail.com>
2023-03-06 05:51:24 +0100

    changed font size on creators page

--- components/DisplaySingleCreator.tsx
+++ components/DisplaySingleCreator.tsx
@@ -28,7 +28,7 @@ const DisplaySingleCreator = ({ isLoading, campaigns }: DisplaySingleCreatorProp
             )}
 
             {!isLoading && campaigns.length === 0 && (
-            <p className="font-epilogue font-semibold text-[14px] leading-[30px] text-[#818183]">
+            <p className="font-epilogue font-semibold text-base md:text-[1.5rem] leading-[30px] text-[#818183]">
                 There's no creator with this id
             </p>
             )}
